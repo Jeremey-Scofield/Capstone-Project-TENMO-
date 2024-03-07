52397713cb92650025ec38b1812b7e454009e531
Jonathan Sinclair <jonathansinclair83@gmail.com>
2024-03-06 19:45:39 -0500

    JS Fixing merge issue

--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/TransferDao.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/TransferDao.java
@@ -1,12 +1,13 @@
-<<<<<<< HEAD
-package com.techelevator.tenmo.dao;public interface TransferDao {
 
-=======
 package com.techelevator.tenmo.dao;
+
+
+
 
 import com.techelevator.tenmo.model.Transfer;
 
 import java.util.List;
+
 
 public interface TransferDao {
 
@@ -19,6 +20,6 @@ public interface TransferDao {
     Transfer receiveTEBucks(Transfer transfer);
 
     int denyTransferRequest(int transfer_type_id);
->>>>>>> eb052d73b15fd14580771eb0b4f9ef7ece1be287
+
 
 }
